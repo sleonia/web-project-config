7605a565189b23958620ee5e67348f4308cc9d1a
sleonia <46787637+sleonia@users.noreply.github.com>
2021-02-18 12:50:31 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     ],
 
     rules: {
-        'indent': ['error', 4],
+        'indent': ['error', 4, {'SwitchCase': 1}],
         semi: 'error',
         quotes: ['error', 'single'],
         'no-multi-spaces': 'error',
