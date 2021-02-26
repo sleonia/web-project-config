d3f120367481ed8fb0d3c007991fb671879644a0
sleonia <46787637+sleonia@users.noreply.github.com>
2021-02-26 12:06:13 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+/* */
+
 module.exports = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
@@ -9,11 +11,63 @@ module.exports = {
         'plugin:@typescript-eslint/recommended'
     ],
 
+    plugins: [
+        'eslint-plugin-no-cyrillic-string'
+    ],
     rules: {
+        '@typescript-eslint/explicit-function-return-type': 2,
         'indent': ['error', 4, {'SwitchCase': 1}],
         semi: 'error',
         quotes: ['error', 'single'],
+        'no-multi-str': 'error',
+        'no-new-func': 'error',
+        'no-proto': 'error',
+        'no-redeclare': 'error',
+        'no-with': 'error',
+        camelcase: ['off', { properties: 'always' }],
+        'eol-last': ['warn', 'always'],
         'no-multi-spaces': 'error',
-        'no-trailing-spaces': 'error'
+        'no-trailing-spaces': 'error',
+        'jsx-quotes': ['error', 'prefer-single'],
+        'no-console': 'error',
+        'no-magic-numbers': 'error',
+        'no-cyrillic-string/no-cyrillic-string': 'error',
+        'key-spacing': ['warn', {
+            beforeColon: false,
+            afterColon: true
+        }],
+
+        'keyword-spacing': ['warn', {
+            before: true,
+            after: true,
+            overrides: {
+                return: { after: true },
+                throw: { after: true },
+                case: { after: true }
+            }
+        }],
+
+        'max-lines': ['warn', {
+            max: 200,
+            skipBlankLines: true,
+            skipComments: true
+        }],
+
+        'multiline-ternary': ['off', 'never'],
+        'no-continue': 'error',
+        'no-tabs': 'error',
+
+        // http://eslint.org/docs/rules/spaced-comment
+        'spaced-comment': ['warn', 'always', {
+            line: {
+                markers: [],
+                exceptions: ['-', '+', '/']
+            },
+            block: {
+                markers: [],
+                exceptions: ['*'],
+                balanced: false
+            }
+        }],
     }
 };
